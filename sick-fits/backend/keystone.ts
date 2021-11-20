27a8ee8e155db09482ae99dc9dfc6862d3f3d7d0
sasha_go <sasha.garcia@gofundme.com>
2021-11-20 09:34:05 -0800

    backend to ch 16

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,14 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
 import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -9,6 +17,16 @@ const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // how long they should stay signed in?
   secret: process.env.COOKIE_SECRET,
 };
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add in initial roles here
+  },
+});
 
 export default config({
   server: {
@@ -25,10 +43,21 @@ export default config({
   lists: createSchema({
     // schema items go in here
     User,
+    Product,
+    ProductImage,
   }),
   ui: {
     // TODO: change this for roles
     isAccessAllowed: () => true,
+
+    // isAccessAllowed: ({ session }) => {
+    //   console.log(session);
+    //   return !!session?.data;
+    // },
   },
   // TODO: add session values here
+  //   session: withItemData(statelessSessions(sessionConfig), {
+  // graphql query
+  // User: 'id',
+  //   }),
 });
